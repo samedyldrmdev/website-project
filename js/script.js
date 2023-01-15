8501367523ef39c5f8279de4364c5334a8fbe8d9
Samet Yıldırım <smtyldrm32@gmail.com>
2023-01-15 23:19:48 +0300

    update

--- js/script.js
+++ js/script.js
@@ -45,3 +45,57 @@ const menuBtn = document.querySelector("#menu-btn");
 menuBtn.addEventListener("click", function(){
     navbar.classList.toggle("active")
 })
+
+
+
+        // function removeelement(){
+        //     document.getElementById("imgbox1").style.display="none";
+        // }
+        // function changeVisibility(){
+        //     document.getElementById("imgbox2").style.visibility="hidden";
+        // }
+        // function reset(){
+        //     document.getElementById("imgbox1").style.display="inline";
+        //     document.getElementById("imgbox2").style.visibility="visible";
+
+        // }
+
+function showp1(){
+    document.getElementById("surec1").style.display="inline-block";
+    document.getElementsById("surec2").style.display="none";
+    document.getElementsById("surec3").style.display="none";
+    document.getElementsById("surec4").style.display="none";
+    document.getElementsById("surec5").style.display="none";
+}
+
+function showp2(){
+    document.getElementsById("surec2").style.display="inline-block";
+    document.getElementById("surec1").style.display="none";
+    document.getElementsById("surec3").style.display="none";
+    document.getElementsById("surec4").style.display="none";
+    document.getElementsById("surec5").style.display="none";
+}
+
+function showp3(){
+    document.getElementById("surec1").style.display="none";
+    document.getElementsById("surec2").style.display="none";
+    document.getElementsById("surec3").style.display="inline-block";
+    document.getElementsById("surec4").style.display="none";
+    document.getElementsById("surec5").style.display="none";
+}
+
+function showp4(){
+    document.getElementById("surec1").style.display="none";
+    document.getElementsById("surec2").style.display="none";
+    document.getElementsById("surec3").style.display="none";
+    document.getElementsById("surec4").style.display="inline-block";
+    document.getElementsById("surec5").style.display="none";
+}
+
+function showp5(){
+    document.getElementById("surec1").style.display="none";
+    document.getElementsById("surec2").style.display="none";
+    document.getElementsById("surec3").style.display="none";
+    document.getElementsById("surec4").style.display="none";
+    document.getElementsById("surec5").style.display="inline-block";
+}
